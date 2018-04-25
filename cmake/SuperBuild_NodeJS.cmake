c5e28c85ef5ee399e7dc5e395779fea150de7d54
Joohwi Lee <jlee@autox.ai>
2018-04-25 11:02:55 -0700

    Added ExternalProject_Add_Step

--- cmake/SuperBuild_NodeJS.cmake
+++ cmake/SuperBuild_NodeJS.cmake
@@ -8,13 +8,23 @@ if (APPLE)
     set (NODEJS_URL_HASH 5c7b05899ff56910a2b8180f139d48612f349ac2c5d20f08dbbeffbed9e3a089)
 endif()
 
-set (NODE_BIN_DIR ${CMAKE_BINARY_DIR}/nodejs/bin)
+set (NODE_ROOT_DIR ${CMAKE_BINARY_DIR}/nodejs)
+set (NODE_BIN_DIR ${NODE_ROOT_DIR}/bin)
+set (NODE_EXECUTABLE ${NODE_BIN_DIR}/node CACHE string "NodeJS executable path")
 
 ExternalProject_Add (nodejs
-    SOURCE_DIR ${CMAKE_BINARY_DIR}/nodejs
+    SOURCE_DIR ${NODE_ROOT_DIR}
     URL ${NODEJS_URL}
     URL_HASH SHA256=${NODEJS_URL_HASH}
-    CONFIGURE_COMMAND TRUE
-    BUILD_COMMAND ${CMAKE_COMMAND} -E ${NODE_BIN_DIR}/node ${NODE_BIN_DIR}/npm i -g json-colorz
-    INSTALL_COMMAND TRUE
+    CONFIGURE_COMMAND "" 
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+)
+
+ExternalProject_Add_Step(nodejs INSTALL_NODE_MODULES
+    WORKING_DIRECTORY ${NODE_BIN_DIR}
+    COMMENT "Installing required node modules"
+    COMMAND ${NODE_EXECUTABLE} ./npm i -g yarn mocha gulp@next pm2 typescript nexe
+    LOG TRUE
+    DEPENDEES install
 )
